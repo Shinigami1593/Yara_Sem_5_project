8948ceba849cfa580b89bcc21fdbc75ca2f8ce49
ayush <rasaileeayush17@gmail.com>
2025-05-18 21:51:29 +0545

    merge conflict fixed but not in a proper manner

---
+++ flutter/yatra_app/lib/view/splash_screen_view.dart
@@ -0,0 +1,41 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:yatra_app/model/splash_model.dart';
+import 'package:yatra_app/view/signin_view.dart';
+// ignore: depend_on_referenced_packages
+import 'package:page_transition/page_transition.dart';
+
+
+class SplashScreenView extends StatelessWidget {
+  final SplashModel _viewmodel = SplashModel();
+
+  SplashScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _viewmodel.initApp(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AnimatedSplashScreen(
+            splash: Lottie.asset('assets/animation/bus_animation.json', height: 200),
+            splashIconSize: 200,
+            backgroundColor: Colors.white,
+            splashTransition: SplashTransition.fadeTransition,
+            duration: 2500,
+            nextScreen: const SignInView(), // Your login screen
+            pageTransitionType: PageTransitionType.bottomToTop,
+          );
+        } else {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: CircularProgressIndicator(color: Color(0xFF07609b)),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
